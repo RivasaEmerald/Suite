891935189658a43719d707291c9103246c4669d4
Sanju Kamat <sanjukamat123@gmail.com>
2021-11-19 11:24:37 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,182 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1",
+      "name": "1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.4002223547409116,
+        "pitch": 0.015537341540891703,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.619587459783018,
+          "pitch": 0.02702842367257574,
+          "rotation": 0,
+          "target": "1-2"
+        },
+        {
+          "yaw": -1.3720326520156316,
+          "pitch": 0.01648548656409865,
+          "rotation": 10.995574287564278,
+          "target": "2-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.1402453224080196,
+          "pitch": -0.2327223128770015,
+          "title": "RIVASA EMERALD SUITE",
+          "text": "<div>Amenities&nbsp;</div><div><br></div><div>- AC</div><div>- TV</div><div>- Sanitized Rooms&nbsp;</div><div>- Daily Housekeeping&nbsp;</div><div>- Free Wifi</div><div>- Swimming pool</div><div>- Roof Top Restaurant &amp; Bar</div><div>- Parking Facility&nbsp;</div><div>- 5 Min drive to the Beach</div><div>- Conference Hall&nbsp;</div><div><br></div>"
+        }
+      ]
+    },
+    {
+      "id": "1-2",
+      "name": "2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.737084395272001,
+        "pitch": 0.17427796005114615,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0001670528856037,
+          "pitch": 0.016569926921738087,
+          "rotation": 0,
+          "target": "0-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-3",
+      "name": "3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.6807811503148127,
+        "pitch": 0.0802638456935334,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.344377957075844,
+          "pitch": 0.03550356948078637,
+          "rotation": 10.995574287564278,
+          "target": "0-1"
+        },
+        {
+          "yaw": -0.19428453579050675,
+          "pitch": 0.2252322041208359,
+          "rotation": 0,
+          "target": "3-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-4",
+      "name": "4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.3733992793655858,
+        "pitch": 0.17941330213849582,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8280337119666665,
+          "pitch": 0.10658957271260228,
+          "rotation": 1.5707963267948966,
+          "target": "2-3"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Rivasa Emerald Suite ",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
